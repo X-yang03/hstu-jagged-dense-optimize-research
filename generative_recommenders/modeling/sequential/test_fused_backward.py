49e28159c90931064c1e2eb972b27654cab457c1
X-yang03 <1979954151@qq.com>
2025-03-24 15:05:45 +0800

    simpler ver backward code

--- generative_recommenders/modeling/sequential/test_fused_backward.py
+++ generative_recommenders/modeling/sequential/test_fused_backward.py
@@ -6,6 +6,8 @@ import random
 import fbgemm_gpu
 import torch.profiler
 from fused_hstu_op import FusedHSTUOp
+from fused_jagged_hstu_backward import fused_jagged_hstu_backward
+from fused_backward_simpler import fused_backward_simpler
 
 def get_input(sum_N, head, d, B, n):
     q = torch.randn(sum_N, head*d, requires_grad=True, device="cuda")
@@ -52,15 +54,17 @@ def origin_einsum_attn(q, k, v, rab, attn_mask, B, n, head, d, x_offsets):
 
 seq_len = [128, 120, 256, 260, 512, 510, 1024, 1020, 100, 200, 300, 400]
 n = 0
-B = 20
+B = 128
 x_offsets = [0]
+max_seq = 200
+min_seq = 100
 for i in range(1, B+1):
-    rand_seq_len = random.choice(seq_len)
+    rand_seq_len = random.randint(min_seq, max_seq)
     n = max(n, rand_seq_len)
     x_offsets.append(x_offsets[-1] + rand_seq_len) # 生成一个长度为B的序列，每个元素为0-1024之间的随机数
 x_offsets = torch.tensor(x_offsets, device="cuda") # 转换为tensor
 
-head, d = 8 , 25
+head, d = 8 , 32
 sum_N = int(x_offsets[-1])
 
 print('benchmark config: sum_N: {}, head: {}, d: {}, B: {}, n: {}'.format(sum_N, head, d, B, n))
@@ -71,16 +75,54 @@ print('input attn_mask shape: ({}, {}, {}, {})'.format(1, 1, n, n))
 
 q, k, v, q1, k1, v1, rab, attn_mask = get_input(sum_N, head, d, B, n)
 einsum_attn = origin_einsum_attn(q, k, v, rab, attn_mask, B, n, head, d, x_offsets)
-fused_attn = FusedHSTUOp.apply(q1, k1, v1, rab, attn_mask, head, d, n, x_offsets)
+# fused_attn = FusedHSTUOp.apply(q1, k1, v1, rab, attn_mask, head, d, n, x_offsets)
 
-print('forward diff:', (torch.abs(einsum_attn - fused_attn)).mean(), torch.max(torch.abs(einsum_attn - fused_attn)))
+# print('forward diff:', (torch.abs(einsum_attn - fused_attn)).mean(), torch.max(torch.abs(einsum_attn - fused_attn)))
 
-loss = einsum_attn.sum()
-loss.backward()
+# loss = einsum_attn.sum()
+# loss.backward()
 
-loss1 = fused_attn.sum()
-loss1.backward()
+# loss1 = fused_attn.sum()
+# loss1.backward()
 
-print('backward diff q:', (q.grad - q1.grad).abs().mean(), (q.grad - q1.grad).abs().max())
-print('backward diff k:', (k.grad - k1.grad).abs().mean(), (k.grad - k1.grad).abs().max())
-print('backward diff v:', (v.grad - v1.grad).abs().mean(), (v.grad - v1.grad).abs().max())
+q, k, v, q1, k1, v1, rab, attn_mask = get_input(sum_N, head, d, B, n)
+
+grad_output = torch.ones_like(einsum_attn)
+dq, dk, dv = fused_jagged_hstu_backward(
+    grad_output, q, k, v, rab, attn_mask, head, d, n, x_offsets
+)
+
+dq1, dk1, dv1 = fused_backward_simpler(
+    grad_output, q1, k1, v1, rab, attn_mask, head, d, n, x_offsets
+)
+
+print('backward diff q1:', (dq - dq1).abs().mean(), (dq - dq1).abs().max())
+print('backward diff k1:', (dk - dk1).abs().mean(), (dk - dk1).abs().max())
+print('backward diff v1:', (dv - dv1).abs().mean(), (dv - dv1).abs().max())
+
+for _ in range(10):
+    start_event = torch.cuda.Event(enable_timing=True)
+    end_event = torch.cuda.Event(enable_timing=True)
+
+    grad_output = torch.randn_like(einsum_attn)
+    q, k, v, q1, k1, v1, rab, attn_mask = get_input(sum_N, head, d, B, n)
+
+    start_event.record()
+    dq, dk, dv = fused_jagged_hstu_backward(
+        grad_output, q, k, v, rab, attn_mask, head, d, n, x_offsets
+    )
+    end_event.record()
+    torch.cuda.synchronize()
+    print('fused_jagged_hstu_backward time:', start_event.elapsed_time(end_event))
+
+    start_event.record()
+    dq1, dk1, dv1 = fused_backward_simpler(
+        grad_output, q1, k1, v1, rab, attn_mask, head, d, n, x_offsets
+    )
+    end_event.record()
+    torch.cuda.synchronize()
+    print('fused_backward_simpler time:', start_event.elapsed_time(end_event))
+
+    print('backward diff q:', (dq - dq1).abs().mean(), (dq - dq1).abs().max())
+    print('backward diff k:', (dk - dk1).abs().mean(), (dk - dk1).abs().max())
+    print('backward diff v:', (dv - dv1).abs().mean(), (dv - dv1).abs().max())
